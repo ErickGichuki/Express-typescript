92e1fc2a81116af05981dc8078736aee346f2fba
+ <+>
2025-01-08 20:41:02 +0300

    refactor:modify the songs model

--- models/User.ts
+++ models/User.ts
@@ -1,60 +1,32 @@
-import { DataTypes, Model, Optional } from 'sequelize';
-import sequelize from '../config/db'; // Adjust the path accordingly
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/db';
 
-// Define an interface for the attributes of the User model
-interface UserAttributes {
-  id: number;
-  username: string;
-  email: string;
-  password: string;
-  role: string;
-}
-
-// Define an interface for optional attributes during creation
-interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
-
-class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
-  public id!: number;
-  public username!: string;
-  public email!: string;
-  public password!: string;
-  public role!: string;
-  
-  public readonly createdAt!: Date;
-  public readonly updatedAt!: Date;
-}
-
-User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    role: {
-      type: DataTypes.STRING,
-      defaultValue: 'member',
-    },
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
   },
-  {
-    sequelize, // Pass the database connection instance
-    modelName: 'User',
-    tableName: 'Users',
-    timestamps: true,
-  }
-);
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    defaultValue: 'member', // Default role
+  },
+}, {
+  tableName: 'Users',
+  timestamps: true,    // Automatically add `createdAt` and `updatedAt`
+});
 
 export default User;
