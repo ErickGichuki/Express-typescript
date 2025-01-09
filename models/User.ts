d362471a14423e766b46e5d6fbaa5b3ac7d4e5cc
+ <+>
2025-01-09 13:40:55 +0300

    refactor:modify the user models and some dependecies

--- models/User.ts
+++ models/User.ts
@@ -1,32 +1,60 @@
-import { DataTypes } from 'sequelize';
+import { DataTypes, Model, Optional } from 'sequelize';
 import sequelize from '../config/db';
 
-const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
+// Attributes for the User model
+interface UserAttributes {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+  role: string;
+}
+
+// Define the User creation attributes (optional fields for new users)
+interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
+
+// Extend Sequelize's Model class to include the UserAttributes and UserCreationAttributes
+class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
+  public id!: number;
+  public username!: string;
+  public email!: string;
+  public password!: string;
+  public role!: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      defaultValue: 'member',
+    },
   },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  role: {
-    type: DataTypes.STRING,
-    defaultValue: 'member', // Default role
-  },
-}, {
-  tableName: 'Users',
-  timestamps: true,    // Automatically add `createdAt` and `updatedAt`
-});
+  {
+    sequelize,
+    tableName: 'Users',
+    timestamps: true,
+  }
+);
 
 export default User;
