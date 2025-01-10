34380874bb53a718d2b26d0e2680253408c1aa88
+ <+>
2025-01-10 15:06:27 +0300

    fix:fix an issue related to tests

--- jest.config.js
+++ jest.config.js
@@ -7,13 +7,14 @@
 //     moduleDirectories: ['node_modules', '<rootDir>'], // Ensure Jest can find the server module
 //   };
   
-  module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/tests/math.tes.ts'],  // Match the test file
-    moduleNameMapper: {
-      '^server$': '<rootDir>/server.ts', // Map "server" to the actual server file
-    },
-    moduleDirectories: ['node_modules', '<rootDir>'],
-  };
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+  moduleNameMapper: {
+    '^server$': '<rootDir>/server.ts',
+  },
+  moduleDirectories: ['node_modules', '<rootDir>'],
+};
+
   
