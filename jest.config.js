3766535c03d7828009aac67be826b0a9733b8796
+ <+>
2025-01-10 15:09:46 +0300

    fix:solve an issue with test

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,4 @@
-// module.exports = {
-//     preset: 'ts-jest',
-//     testEnvironment: 'node',
-//     moduleNameMapper: {
-//       '^server$': '<rootDir>/server.ts', // Map "server" to the actual server file
-//     },
-//     moduleDirectories: ['node_modules', '<rootDir>'], // Ensure Jest can find the server module
-//   };
-  
-module.exports = {
+  module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
