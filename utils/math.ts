35c90b01007d0d3024babf6c3aad5139c5825c83
+ <+>
2025-01-11 19:20:16 +0300

    refactor:modify tests

---
+++ utils/math.ts
@@ -0,0 +1,12 @@
+// utils/math.ts
+
+/**
+ * Adds two numbers together.
+ * @param a - The first number.
+ * @param b - The second number.
+ * @returns The sum of the two numbers.
+ */
+export const add = (a: number, b: number): number => {
+    return a + b;
+  };
+  
